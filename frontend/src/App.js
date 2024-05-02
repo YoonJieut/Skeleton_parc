7fe5b84f9cd685e9c739dd3448a6f4ff781ca0fb
YoonJunHyeon <qmdldos@gmail.com>
2024-05-02 11:48:02 +0900

    Feat : Controller 생성하고 spa 흐름 파악해보기
    상태에 따라을 변경하지 않고, 컴포넌트를 띄운다. 이거 페이지네이션에 가깝기 보단, 일부 컴포넌트의 테크닉에 가깝지 않을까?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">hello world</div>;
-}
+import { useState, useTransition } from "react";
+import TabButton from "./TabButton.js";
+import AboutTab from "./AboutTab.js";
+import PostsTab from "./PostsTab.js";
+import ContactTab from "./ContactTab.js";
 
-export default App;
+export default function TabContainer() {
+  const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState("about");
+
+  function selectTab(nextTab) {
+    startTransition(() => {
+      setTab(nextTab);
+    });
+  }
+
+  return (
+    <>
+      <TabButton isActive={tab === "about"} onClick={() => selectTab("about")}>
+        About
+      </TabButton>
+      <TabButton isActive={tab === "posts"} onClick={() => selectTab("posts")}>
+        Posts (slow)
+      </TabButton>
+      <TabButton
+        isActive={tab === "contact"}
+        onClick={() => selectTab("contact")}
+      >
+        Contact
+      </TabButton>
+      <hr />
+      {tab === "about" && <AboutTab />}
+      {tab === "posts" && <PostsTab />}
+      {tab === "contact" && <ContactTab />}
+    </>
+  );
+}
