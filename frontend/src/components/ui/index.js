6374686b3145490d45dfbffc9c58c8a2390a69eb
YoonJieut <qmdldos@gmail.com>
2024-05-07 22:37:32 +0900

    Delete : 삭제

--- frontend/src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Sectionabout } from './Sectionabout';
-export { default as Popup } from './Popup';
-export { default as PaperBtn } from './PaperBtn';
-export { default as Btn } from './Btn';
-export { default as PhotoText } from './PhotoText';
-export { default as Image } from './Image';
