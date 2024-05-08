112d277254d1649e8324889883c78af9b6afa916
YoonJunHyeon <qmdldos@gmail.com>
2024-05-08 15:46:18 +0900

    feat : fetch를 통한 api 요청 예시 코드 작성
    type과 consle.log를 통해 알아볼 수 있게하고 반환도하도록 설정해놓았다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React, { Suspense, useState } from "react";
-import TabButton from "./TabButton"; // TabButton 컴포넌트를 import합니다.
-const AboutTab = React.lazy(() => import("./AboutTab"));
-const PostsTab = React.lazy(() => import("./PostsTab"));
-const ContactTab = React.lazy(() => import("./ContactTab"));
+import TabButton from "./components/TabButton"; // TabButton 컴포넌트를 import합니다.
+const AboutTab = React.lazy(() => import("./components/AboutTab"));
+const PostsTab = React.lazy(() => import("./components/PostsTab"));
+const ContactTab = React.lazy(() => import("./components/ContactTab"));
 
 export default function TabContainer() {
   const [tab, setTab] = useState("about");
+  const handleApiRequest = () => {
+    // Send API request here
+    console.log("API request sent");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => {
+        console.log(response);
+        return response.json(); // 2번째 then에 전달과 데이터 표시를 위한 결과값 반환
+      })
+      .then((response) => console.log(`type : ${typeof response}`))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <Suspense fallback={<h1>🌀 Loading...</h1>}>
       <TabButton isActive={tab === "about"} onClick={() => setTab("about")}>
@@ -17,6 +29,7 @@ export default function TabContainer() {
       <TabButton isActive={tab === "contact"} onClick={() => setTab("contact")}>
         Contact
       </TabButton>
+      <button onClick={handleApiRequest}>Send API Request</button>
       <hr />
       {tab === "about" && <AboutTab />}
       {tab === "posts" && <PostsTab />}
