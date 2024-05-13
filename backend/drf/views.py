f64e13ec3b23590c4b370a42842140b959e91bc8
YoonJunHyeon <qmdldos@gmail.com>
2024-05-13 18:12:59 +0900

    Init : drf 앱에 예시 파일 추가
    models, urls, views를 추가해주었다.

--- backend/drf/views.py
+++ backend/drf/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# Django REST framework를 사용하여 API 뷰를 생성할 수 있음. 필요하다면 pip install djangorestframework로 설치
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Event
+from .serializers import EventSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
