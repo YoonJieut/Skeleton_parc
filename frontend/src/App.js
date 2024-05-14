ce027e5ba947edbc8e13918c2c78e59e4dd5469e
YoonJunHyeon <qmdldos@gmail.com>
2024-05-14 17:00:38 +0900

    Feat : api 테스트를 위한 fetch 하는 컴포넌트로 변경
    로드하면 백앤드서버로 보내, api 요청을 연결해놓았다. 현재 백앤드 서버를 테스트 중이다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,28 @@
-import React, { Suspense, useState } from "react";
-import TabButton from "./components/TabButton"; // TabButton 컴포넌트를 import합니다.
-const AboutTab = React.lazy(() => import("./components/AboutTab"));
-const PostsTab = React.lazy(() => import("./components/PostsTab"));
-const ContactTab = React.lazy(() => import("./components/ContactTab"));
+import React, { useEffect, useState } from "react";
 
-export default function TabContainer() {
-  const [tab, setTab] = useState("about");
-  const handleApiRequest = () => {
-    // Send API request here
-    console.log("API request sent");
-    fetch("http://127.0.0.1:5000/hello")
-      .then((response) => {
-        console.log(response);
-        return response.json(); // 2번째 then에 전달과 데이터 표시를 위한 결과값 반환
-      })
-      // 이제 모든 과정이 완료된 후 결과값을 보기 위한 console.log를 추가했습니다.
-      .then((response) => console.log(`type : ${typeof response}`, response))
-      .catch((error) => console.error(error));
-  };
+function App() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/drf/")
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
-    <Suspense fallback={<h1>🌀 Loading...</h1>}>
-      <TabButton isActive={tab === "about"} onClick={() => setTab("about")}>
-        About
-      </TabButton>
-      <TabButton isActive={tab === "posts"} onClick={() => setTab("posts")}>
-        Posts
-      </TabButton>
-      <TabButton isActive={tab === "contact"} onClick={() => setTab("contact")}>
-        Contact
-      </TabButton>
-      <button onClick={handleApiRequest}>Send API Request</button>
-      <hr />
-      {tab === "about" && <AboutTab />}
-      {tab === "posts" && <PostsTab />}
-      {tab === "contact" && <ContactTab />}
-    </Suspense>
+    <>
+      <h1>메인페이지 로딩 완료</h1>
+      <div>
+        {events.map((event) => (
+          <div key={event.id}>
+            <h1>{event.title}</h1>
+            <p>{event.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
+
+export default App;
