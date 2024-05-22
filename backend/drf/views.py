5c099f132ba74613528491aa965473703d99389f
YoonJunHyeon <qmdldos@gmail.com>
2024-05-22 13:19:01 +0900

    try : url 변수에 미리 저장해두어서 그런가 다른 방식으로 시도해본다.

--- backend/drf/views.py
+++ backend/drf/views.py
@@ -5,22 +5,23 @@
 # from .serializers import EventSerializer
 
 # fetch_cultural_events의 경우 requests 라이브러리를 사용하여 외부 API로부터 데이터를 가져옴
-from django.http import JsonResponse
 import requests
+from django.http import JsonResponse
 
 
 # class EventViewSet(viewsets.ModelViewSet):
 #     queryset = Event.objects.all()
 #     serializer_class = EventSerializer
 
+    
 def fetch_cultural_events(request):
-    url = "http://www.culture.go.kr/openapi/rest/publicperformancedisplays/area?ServiceKey=XAQRkAc4BBj5RWTWfdEX5Oc5ry0o4j74tmxD5R4HhJoegNLrNTlRZl6%2BCW%2BX%2BC28DAwBYq73UvcNcdB6n591bg%3D%3D&amp;cPage=1&amp;rows=999"
-    response = requests.get(url)
     try:
-        data = response.json()
-        return JsonResponse(data)
+        response = requests.get('http://www.culture.go.kr/openapi/rest/publicperformancedisplays/area?ServiceKey=XAQRkAc4BBj5RWTWfdEX5Oc5ry0o4j74tmxD5R4HhJoegNLrNTlRZl6%2BCW%2BX%2BC28DAwBYq73UvcNcdB6n591bg%3D%3D&amp;cPage=1&amp;rows=999')
+        response.raise_for_status()  # HTTP 오류가 발생할 경우 예외 발생
+        data = response.json()  # 여기서 JSON 디코딩 시도
     except ValueError as e:
-        print("Response Content:", response.content)  # 응답 내용 출력
-        raise ValueError("Unable to decode JSON.") from e
+        return JsonResponse({'error': 'Unable to decode JSON'}, status=400)
+    except requests.RequestException as e:
+        return JsonResponse({'error': str(e)}, status=response.status_code)
     
-    
+    return JsonResponse(data, safe=False)
